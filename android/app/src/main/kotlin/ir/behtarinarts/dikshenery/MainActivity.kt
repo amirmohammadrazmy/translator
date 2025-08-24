86d71262add94fbf0bdce211459c8056c674c26b
Amirmohammad Razmy <behtarinarts@gmail.com>
2025-08-24 22:57:32 +0330

    test 3

--- android/app/src/main/kotlin/ir/behtarinarts/dikshenery/MainActivity.kt
+++ android/app/src/main/kotlin/ir/behtarinarts/dikshenery/MainActivity.kt
@@ -3,4 +3,5 @@ package ir.behtarinarts.dikshenery
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    // Android v2 embedding is automatically handled by FlutterActivity
 }
